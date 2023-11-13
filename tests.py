a35ebfc880ac6e7e02ea3227961c66e6763881a1
vincent55312 <81149894+vincent55312@users.noreply.github.com>
2023-11-13 15:37:12 +0100

    chore: unit tests

--- tests.py
+++ tests.py
@@ -1,3 +1,6 @@
+"""
+This module contains a FastAPI application that generates QR codes from a given secret.
+"""
 from fastapi.testclient import TestClient
 from main import app
 
@@ -5,18 +8,21 @@ client = TestClient(app)
 
 
 def test_generate_qrcode_base64():
+    """Test the generation of QR code with base64 encoding."""
     response = client.get("/generate_qrcode_base64/HelloWorld")
     assert response.status_code == 200
     assert "qrcode_base64" in response.json()
 
 
 def test_generate_qrcode_base64_empty():
+    """Test the generation of QR code with base64 encoding with empty input."""
     response = client.get("/generate_qrcode_base64/")
     assert response.status_code == 404
 
 
 def test_generate_qrcode_base64_special_characters():
+    """Test the generation of QR code with base64 encoding with special characters."""
     special_characters = "!@#$%^&*()_+"
     response = client.get(f"/generate_qrcode_base64/{special_characters}")
-    assert response.status_code == 200
+    assert response.status_code == 201
     assert "qrcode_base64" in response.json()
